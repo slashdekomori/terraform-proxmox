cc4fba55f8202ccbe67f7aed9591b64d449a0ee3
slashdekomori <slashdekomori@proton.me>
2026-07-27 13:44:31 +0300

    image scripts

---
+++ images/debian-13-cloud.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+wget https://cloud.debian.org/images/cloud/trixie/latest/debian-13-genericcloud-amd64.qcow2
+
+cp debian-13-genericcloud-amd64.qcow2 debian-13-genericcloud-amd64-custom.qcow2
+
+virt-customize -a debian-13-genericcloud-amd64-custom.qcow2 \
+    --install qemu-guest-agent,gnupg \
+    --run-command 'systemctl enable qemu-guest-agent'
