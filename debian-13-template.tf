cc337600db55a6e7a112a07534980b51852af0a1
slashdekomori <slashdekomori@proton.me>
2026-07-27 13:44:37 +0300

    debian13 template

---
+++ debian-13-template.tf
@@ -0,0 +1,78 @@
+resource "proxmox_virtual_environment_vm" "debian-13-template" {
+  name      = "debian-13-template"
+  node_name = "psyche"
+  vm_id     = 9100
+
+  cpu {
+    cores   = 1
+    sockets = 1
+    type    = "host"
+  }
+
+  memory {
+    dedicated = 1024
+    floating  = 1024
+  }
+
+  initialization {
+    datastore_id = "slow"
+
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+
+    user_account {
+      username = "dekomori"
+      password = var.vm_password
+      keys     = var.ssh_keys
+    }
+  }
+
+  disk {
+    datastore_id = "slow"
+    import_from  = var.debian13-image
+    interface    = "virtio0"
+    iothread     = true
+    backup       = false
+    discard      = "on"
+  }
+
+  serial_device {
+    device = "socket"
+  }
+
+  vga {
+    type = "serial0"
+  }
+
+  machine = "q35"
+  bios    = "ovmf"
+
+  operating_system {
+    type = "l26"
+  }
+
+  efi_disk {
+    datastore_id = "slow"
+    type         = "4m"
+  }
+
+  agent {
+    enabled = true
+    wait_for_ip {
+      disabled = true
+    }
+  }
+
+  network_device {
+    bridge = "vmbr0"
+    model  = "virtio"
+  }
+
+  template        = true
+  started         = false
+  stop_on_destroy = true
+}
+
