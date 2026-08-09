606ba763c7270dc6942737bbdfcc8a535732f2ea
slashdekomori <slashdekomori@proton.me>
2026-08-09 21:45:00 +0300

    new variable for ssh auth
    variables correction

--- variables.tf
+++ variables.tf
@@ -8,26 +8,13 @@ variable "proxmox_api_token" {
   type        = string
   sensitive   = true
 }
-
+variable "proxmox_password" {
+  description = "Proxmox VE API token"
+  type        = string
+  sensitive   = true
+}
 variable "proxmox_insecure" {
   description = "Skip TLS verification for Proxmox API"
   type        = bool
   default     = true
-}
-
-variable "vm_password" {
-  description = "VM user account password "
-  type        = string
-  sensitive   = true
-}
-
-variable "ssh_keys" {
-  description = "List of SSH public keys"
-  type        = list(string)
-  default     = []
-}
-
-variable "debian13-image" {
-  description = "Path to debian13-image"
-  type        = string
 }
