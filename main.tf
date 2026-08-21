cb96fb8bce1d85f3a67d91db67cbbe3388891a4b
slashdekomori <slashdekomori@proton.me>
2026-08-21 11:59:12 +0300

    adapt to module change and migrate to local & for each approach

--- main.tf
+++ main.tf
@@ -20,18 +20,34 @@ provider "proxmox" {
   }
 }
 
-module "debian13_template" {
-  source = "./modules/vm-template"
+resource "proxmox_download_file" "debian-13-genericcloud-amd64" {
+  content_type = "import"
+  datastore_id = "local"
+  node_name    = "psyche"
+  url          = "https://cloud.debian.org/images/cloud/trixie/latest/debian-13-genericcloud-amd64.qcow2"
+}
 
-  node_name       = "psyche"
-  vm_id           = 9100
-  datastore_id    = "slow"
-  vm_name         = "debian13-template"
-  cloud_image_url = "https://cloud.debian.org/images/cloud/trixie/latest/debian-13-genericcloud-amd64.qcow2"
+locals {
+  default_datastore = "slow"
+  default_node_name = "psyche"
 
+  templates = {
+    debian13_template_1 = { vm_id = 9100, vm_name = "debian13-template" }
+    debian13_template_2 = { vm_id = 9200, vm_name = "debian13-template-2" }
+  }
+}
+
+module "vm_template" {
+  source   = "./modules/vm-template"
+  for_each = local.templates
+
+  node_name       = try(each.value.datastore_id, local.default_node_name)
+  vm_id           = each.value.vm_id
+  datastore_id    = try(each.value.datastore_id, local.default_datastore)
+  vm_name         = each.value.vm_name
+  cloud_image_id  = proxmox_download_file.debian-13-genericcloud-amd64.id
   ssh_public_keys = var.ssh_public_keys
   user_password   = var.user_password
   user_name       = var.user_name
   timezone        = var.timezone
 }
-
