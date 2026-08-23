027bbfea090322c801bfce916364b4edbdb029b3
slashdekomori <slashdekomori@proton.me>
2026-08-23 21:56:08 +0300

    idk

--- main.tf
+++ main.tf
@@ -32,18 +32,18 @@ locals {
   default_node_name = "psyche"
 
   vms = {
-    test_vm_1 = { vm_id = 9100 }
-    test_vm_2 = { vm_id = 9200 }
+    test-vm-1 = { vm_id = 1100 }
+    test-vm-2 = { vm_id = 1200 }
   }
 
-  templates = {
-    debian13_template_1 = { vm_id = 9100 }
+  vm_templates = {
+    debian13-template = { vm_id = 9100 }
   }
 }
 
 module "vm_template" {
-  source   = "./modules/vm-template"
-  for_each = local.templates
+  source   = "./modules/vm_template"
+  for_each = local.vm_templates
 
   node_name       = try(each.value.datastore_id, local.default_node_name)
   vm_id           = each.value.vm_id
