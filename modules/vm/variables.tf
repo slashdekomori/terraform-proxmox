4d0a8515b8ce1143373383e0609d6e59778abd9b
slashdekomori <slashdekomori@proton.me>
2026-08-24 10:49:11 +0300

    vm_id is now optional

--- modules/vm/variables.tf
+++ modules/vm/variables.tf
@@ -2,8 +2,10 @@ variable "cloud_image_id" { type = string }
 variable "node_name" { type = string }
 variable "datastore_id" { type = string }
 variable "vm_name" { type = string }
-# TODO: Make optional, so if not specified will use any free id
-variable "vm_id" { type = number }
+variable "vm_id" {
+  type    = number
+  default = null
+}
 variable "disk_size" {
   type    = number
   default = 20
