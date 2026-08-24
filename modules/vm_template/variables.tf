07a9b868823cbf904db875140659065368a88c20
slashdekomori <slashdekomori@proton.me>
2026-08-24 10:49:21 +0300

    clean comment

--- modules/vm_template/variables.tf
+++ modules/vm_template/variables.tf
@@ -2,7 +2,6 @@ variable "cloud_image_id" { type = string }
 variable "node_name" { type = string }
 variable "datastore_id" { type = string }
 variable "vm_name" { type = string }
-#TODO: Make optional, so if not specified will use any free id
 variable "vm_id" { type = number }
 variable "disk_size" {
   type    = number
