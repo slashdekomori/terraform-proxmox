8731a9ff08ab8ed296592e4b3f706d651e6f8773
slashdekomori <slashdekomori@proton.me>
2026-09-18 20:50:53 +0300

    config change, adapted to shared template id vars

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,7 @@
 locals {
-  default_image_file_id = proxmox_download_file.debian-13-genericcloud-amd64.id
+  templates            = jsondecode(file("${path.root}/../shared/templates.json"))
+  debian13_template_id = local.templates.debian13_template_id
+  default_template_id  = local.debian13_template_id
 
   vms = {
     test-vm1 = {}
@@ -22,12 +24,15 @@ module "vm_template" {
   source   = "../modules/vm_template"
   for_each = local.vm_templates
 
-  node_name       = try(each.value.node_name, var.default_node_name)
-  vm_id           = try(each.value.vm_id, null)
-  datastore_id    = try(each.value.datastore_id, var.default_datastore_id)
-  vm_name         = try(each.value.vm_name, each.key)
-  image_file_id   = try(each.value.image_file_id, local.default_image_file_id)
-  ssh_public_keys = var.ssh_public_keys
+  node_name    = try(each.value.node_name, var.default_node_name)
+  vm_id        = try(each.value.vm_id, null)
+  datastore_id = try(each.value.datastore_id, var.default_datastore_id)
+  vm_name      = try(each.value.vm_name, each.key)
+  # TODO: kinda sketchy
+  image_file_id   = try(each.value.image_file_id, null)
+  user_name       = try(each.value.user_name, var.init_user_name)
+  user_password   = try(each.value.user_password, var.init_user_password)
+  ssh_public_keys = var.init_ssh_public_keys
 }
 
 module "vm" {
@@ -38,7 +43,9 @@ module "vm" {
   vm_id         = try(each.value.vm_id, null)
   datastore_id  = try(each.value.datastore_id, var.default_datastore_id)
   vm_name       = try(each.value.vm_name, each.key)
-  image_file_id = try(each.value.image_file_id, local.default_image_file_id)
+  image_file_id = try(each.value.image_file_id, null)
+  template_id   = try(each.value.image_file_id, local.default_template_id, null)
+
 
   user_name       = try(each.value.user_name, var.default_user_name)
   user_password   = try(each.value.user_password, var.default_user_password)
